95f943aaf13e64ac13b7ba47a71a06536293f43a
Byungchan-Park <pbc9236@gmail.com>
2020-10-08 20:12:25 +0900

    refactor: Use components of Formik

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useFormik } from "formik"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 import * as Yup from "yup"
 
 const initialValues = {
@@ -19,35 +19,28 @@ const validationSchema = Yup.object({
 })
 
 function YoutubeForm() {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-    // validate,
-  })
-
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" {...formik.getFieldProps("name")} autoComplete="off" />
-          {formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
+          <Field type="text" id="name" name="name" autoComplete="off" />
+          <ErrorMessage name="name" />
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" {...formik.getFieldProps("email")} autoComplete="off" />
-          {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
+          <Field type="email" id="email" name="email" autoComplete="off" />
+          <ErrorMessage name="email" />
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" {...formik.getFieldProps("channel")} autoComplete="off" />
-          {formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null}
+          <Field type="text" id="channel" name="channel" autoComplete="off" />
+          <ErrorMessage name="channel" />
         </div>
 
         <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   )
 }
 
