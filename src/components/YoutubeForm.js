972b7f78936fbf67efb7b2804e87c2b28dc3728b
Byungchan-Park <pbc9236@gmail.com>
2020-10-08 19:23:59 +0900

    feat: Add the code which improves validation UX using the state about onBlur event

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -42,25 +42,25 @@ function YoutubeForm() {
     validate,
   })
 
-  console.log("Formik errors", formik.errors)
+  console.log("Visited fields", formik.touched)
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
-          {formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
+          <input type="text" id="name" name="name" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} autoComplete="off" />
+          {formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
         </div>
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
-          {formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
+          <input type="email" id="email" name="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} autoComplete="off" />
+          {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
-          {formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null}
+          <input type="text" id="channel" name="channel" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.channel} autoComplete="off" />
+          {formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null}
         </div>
 
         <button type="submit">Submit</button>
