66a6d78a08ab2a15d7b9a147ce371519be3f2225
Byungchan-Park <pbc9236@gmail.com>
2020-10-08 18:00:32 +0900

    refactor: simplify the useFormik's argument

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,38 +1,45 @@
 import React from "react"
 import { useFormik } from "formik"
 
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+}
+
+const onSubmit = (values) => {
+  console.log("Form data", values)
+}
+
+const validate = (values) => {
+  // values.name values.email values.channel
+  // errors.name errors.email errors.channel
+  // errors.name = 'This field is required'
+  let errors = {}
+
+  if (!values.name) {
+    // if name is empty
+    errors.name = "Required"
+  }
+
+  if (!values.email) {
+    errors.email = "Required"
+  } else if (/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(values.email)) {
+    error.email = "Invalid email format"
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required"
+  }
+
+  return errors
+}
+
 function YoutubeForm() {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form data", values)
-    },
-    validate: values => {
-      // values.name values.email values.channel
-      // errors.name errors.email errors.channel
-      // errors.name = 'This field is required'
-      let errors = {}
-
-      if (!values.name) { // if name is empty
-        errors.name = 'Required'
-      }
-
-      if (!values.email) {
-        errors.email = 'Required'
-      } else if (/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(values.email)) {
-        error.email = 'Invalid email format'
-      }
-
-      if (!values.channel) {
-        errors.channel = 'Required'
-      }
-
-      return errors;
-    }
+    initialValues,
+    onSubmit,
+    validate,
   })
 
   return (
